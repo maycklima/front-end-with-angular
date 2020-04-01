d007c9631f5a610ac9e0939e0c3c654466b8f06a
Mayck Lima <mayckclima@gmail.com>
2020-04-01 13:39:13 -0300

    adicionado

--- src/app/data-bind/data-bind.component.ts
+++ src/app/data-bind/data-bind.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-bind.component.css']
 })
 export class DataBindComponent implements OnInit {
-
+  urlImage = 'https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/2/5/7/4/2574f9070ce48b988fe2693a60c40427.jpg';
   constructor() { }
 
   ngOnInit(): void {
