dceb8c3252367534c73ecf2d1bb67c49c8b13b07
Mayck Lima <mayckclima@gmail.com>
2020-04-02 21:20:17 -0300

    top

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DataBindComponent } from './data-bind/data-bind.component';
+import { InputPropertyComponent } from './input-property/input-property.component';
+import { OutputPropertyComponent } from './output-property/output-property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindComponent
+    DataBindComponent,
+    InputPropertyComponent,
+    OutputPropertyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
